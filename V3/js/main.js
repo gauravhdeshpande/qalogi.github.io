edd5b84889e8b1a154ccbda9ecf7d0f61d918f08
KD <koustubh.desai@logixal.com>
2017-11-20 18:36:14 +0530

    Added fonts and bootstrap css

--- V3/js/main.js
+++ V3/js/main.js
@@ -1,4 +1,35 @@
 
-var app = angular.module('LogixalApp',[]).controller('LeftPane',[function(){
-console.log('hie');
+var app = angular.module('LogixalApp',[])
+.controller('LeftPane',["$scope",function($sc){
+console.log('hie',$sc);
+$sc.pages=[{
+	"name":"What We Do",
+	"url" :"/home",
+	"submenu":[{
+		"name":"Jack",
+		"url" :"/home"
+	},{
+		"name":"Sparrow",
+		"url" :"/home"
+	}]
+},{
+	"name":"Company",
+	"url" :"/about-us"
+},{
+	"name":"Contact Us",
+	"url" :"/about-us"
+},
+{
+	"name":"Join Us",
+	"url" :"/about-us"
+},{
+	"name":"Case Studies",
+	"url" :"/about-us"
+}];
+}]);
+app.controller('Root',["$scope",function($sc){
+	console.log('i am grut');
+}]);
+app.controller('Stage',["$scope",function($sc){
+	console.log('i am stageee');
 }])
