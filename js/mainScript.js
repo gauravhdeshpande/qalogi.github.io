f3eef9dde0bbd036aaf382d19ddd02d48ccd907f
KD <koustubh.desai@logixal.com>
2017-11-28 13:07:42 +0530

    Added mainStyle and mainScript to all pages

--- js/mainScript.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$(document).on('click','.panel',function(e){
-		var t = $(e.target).parent().parent().next('.panel-collapse');
-		console.log($(t).hasClass('in'));
-	})
-})
