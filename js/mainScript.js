e641e32c4b7e67a7d74a481ce13746a7b6d6a140
KD <koustubh.desai@logixal.com>
2017-12-06 14:04:56 +0530

    fixed button

--- js/mainScript.js
+++ js/mainScript.js
@@ -4,13 +4,14 @@ $(document).ready(function(){
 	
 	$('.SeeMore2').click(function() {
       var $this = $(this);
+	  var txt = $($this.children('.btn-text')[0]);
 	  $this.toggleClass('up');
       $this.toggleClass('SeeMore2');
       if ($this.hasClass('SeeMore2')) {
-        $this.text('View More ');
+        txt.text('View More ');
         //$this.append('<img src="images/down-arrow.png" alt="Arrow down" />');
       } else {
-        $this.text('View Less ');
+        txt.text('View Less ');
         //$this.append('<img src="images/up-arrow.png" alt="Arrow Up" />');
       }
     });
